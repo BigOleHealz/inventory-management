7a24a4a6eaf91fbddcac34cdd6e22c59fca5f753
bigolehealz <matt.t.healy1994@gmail.com>
2024-08-29 14:43:40 -0400

    backend works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,32 @@
-// import React, { useEffect, useState } from 'react';
-
-// function App() {
-//   const [message, setMessage] = useState('');
-
-//   useEffect(() => {
-//     fetch('http://127.0.0.1:5000/api/test')
-//       .then(response => response.json())
-//       .then(data => setMessage(data.message));
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <h1>{message}</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React from 'react';
-import Dashboard from './Dashboard';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/test')
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
   return (
     <div className="App">
-      <Dashboard />
+      <h1>{message}</h1>
     </div>
   );
 }
+
+// export default App;
+
+// import React from 'react';
+// import Dashboard from './Dashboard';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Dashboard />
+//     </div>
+//   );
+// }
 
 export default App;
